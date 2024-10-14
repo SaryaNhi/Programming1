5a6a270cddcd655db164d40d48179061063eb273
Ryy <146813476+SaryaNhi@users.noreply.github.com>
2024-10-14 06:27:08 +0000

    helloo

---
+++ Code_W1/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int hello(const char *name)
+{
+    printf("hello %s\n", name);
+}
---
+++ Code_W1/hello_main.c
@@ -0,0 +1,8 @@
+void hello_world();
+void hello_student();
+
+void main()
+{
+    hello_world();
+    hello_student();
+}
